648417d57463e589cdfdbf2841602b6eb64795d1
c9zA <177842201@qq.com>
2026-09-23 03:01:42 -0400

    Add: word-ladder - submission-3

---
+++ Data Structures & Algorithms/word-ladder/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        letters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+        wordSet = set(wordList)
+        if endWord not in wordSet:
+            return 0
+        q = deque()
+        seen = set()
+        q.append((beginWord, 1))
+        while q:
+            word, layer = q.popleft()
+            step = False
+            for i in range(len(word)):
+                for c in letters:
+                    temp = word[:i]
+                    temp += c
+                    if i+1<len(word):
+                        temp +=word[i+1:]
+                    if temp in wordSet and temp not in seen:
+                        seen.add(temp)
+                        q.append((temp, layer+1))
+                        if temp==endWord:
+                            return layer+1
+        return 0
